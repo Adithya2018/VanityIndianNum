6c0e157361b16f2b22dd0f8ee2442defb6f12347
Adithya2018 <adithyansraj20@gmail.com>
2023-07-17 19:13:41 +0530

    used pandas instead of openpyxl for reading excel

--- vanity_number_library.py
+++ vanity_number_library.py
@@ -1,24 +1,20 @@
 import re
 import openpyxl as opxl
 from vanity_indian_no import VanityIndianNo
+import pandas as pd
 
 
 if __name__ == '__main__':
     
     # Taking the rule book for Vanity Check as input
-    vanity_check_level_cost_wb = opxl.load_workbook("Data/Vanity_Check_bsnl_cost.xlsx")
-    vanity_check_level_cost_sheet_names = vanity_check_level_cost_wb.sheetnames
-    vanity_check_level_cost_ws = vanity_check_level_cost_wb[vanity_check_level_cost_sheet_names[0]]
+    vanity_check_level_cost_df = pd.read_excel("Data/Vanity_Check_bsnl_cost.xlsx", sheet_name='Sheet1')
 
     # Taking phone numbers as input from workbook
-    vanity_check_wb = opxl.load_workbook("Data/vanity check.xlsx")
-    vanity_check_sheet_names = vanity_check_wb.sheetnames
-    vanity_check_ws = vanity_check_wb[vanity_check_sheet_names[0]]
+    input_phone_nos_df = pd.read_excel("Data/vanity check.xlsx", sheet_name='Input_Sheet')
     
-    for i in range(2, vanity_check_ws.max_row):
-        
-        input_phone_no = vanity_check_ws.cell(row = i, column = 1)
-        vn = VanityIndianNo(str(input_phone_no.value))
+    for input_phone_no in input_phone_nos_df['External_id']:
+
+        vn = VanityIndianNo(str(input_phone_no))
 
         if vn.is_valid_no(vn.phone_no):
             level, rate = vn.pattern_check(vn.phone_no)
