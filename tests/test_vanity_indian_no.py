196fc999f6c8790dd34224a513050b241156f7e6
Adithya2018 <adithyansraj20@gmail.com>
2023-08-14 18:46:15 +0530

    fixed pytest issue

--- tests/test_vanity_indian_no.py
+++ tests/test_vanity_indian_no.py
@@ -1,10 +1,10 @@
 import pytest
 
-from VanityIndianNum.vanity_indian_no import VanityIndianNo as vin
+from VanityIndianNum.vanity_indian_no import VanityIndianNo as Vin
 
 
 def test_is_valid_no():
     """ Checks if the no is valid """
     expected_answers = [False, True]
-    actual_answers = [vin.is_valid_no('2342343242323543'), vin.is_valid_no('9447231411')]
+    actual_answers = [Vin.is_valid_no('2342343242323543'), Vin.is_valid_no('9447231411')]
     assert expected_answers == actual_answers
